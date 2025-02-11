5c078cd7b86170c7b0cc801ba531c25b154478a5
Aayush-Choudhary-7654 <opgaming7654@gmail.com>
2025-02-11 19:02:23 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,27 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="letter ">
-        <div className="bg-[#111] h-[500px] w-[500px] m-auto rounded-[5%] relative top-50">
-          <div className="bg-[crimson] absolute m-auto h-[500px] w-[50px] inset-x-0 top-0"></div>
-          <div className="bg-[crimson] absolute m-auto h-[50px] w-[500px] inset-x-0 top-55"></div>
+      <div className="letter z-1 ">
+        <div className="bg-[#111] h-[300px] w-[300px] m-auto rounded-[5%] relative top-50">
+          <div className="bg-[crimson] absolute m-auto h-[300px] w-[50px] inset-x-0 top-0"></div>
+          <div className="bg-[crimson] absolute m-auto h-[50px] w-[300px] inset-x-0 top-30"></div>
         </div>
-        <div className="bg-[#111] absolute m-auto h-[70px] w-[500px] inset-x-0 top-50 rounded-tl-[23px] rounded-tr-[23px]">
+        <div className="bg-[#111] absolute m-auto h-[70px] w-[300px] inset-x-0 top-50 rounded-tl-[23px] rounded-tr-[23px]">
           <div className="bg-[crimson] absolute m-auto h-[70px] w-[50px] inset-x-0 top-0"></div>
           
         </div>
-        <div class="relative w-20 h-20">
-            <div class="absolute top-[10px] bg-black left-0 w-54 h-50  rounded-full"></div>
-            <div class="absolute right-0 w-54 h-50 inset-x-10 inset-y-10 bg-black rounded-full"></div>
-          </div>
+        
+      </div>
+      <div className="bow-tie relative top-10 -z-10 ">
+      <div class="absolute w-20 h-20 top-[-300px] left-[-100px]">
+            <div class="absolute bg-black w-50 left-[280px] h-50 rounded-full"></div>
+            <div class="absolute bg-black w-50 left-[150px] h-50 rounded-full"></div>
+      
+      <div class="absolute w-20 h-20 left-[10px]">
+            <div class="absolute bg-white w-42 top-1.5 left-[280px] h-46 rounded-full"></div>
+            <div class="absolute bg-white w-42 top-1.5 left-[160px] h-46 rounded-full"></div>
+      </div>
+      </div>
       </div>
     </>
   );
